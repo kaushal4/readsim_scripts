84e6f698b933dbe97012800cf451179a936acbf6
kaushal <kaushal>
2024-10-21 19:32:36 -0400

    initial script

---
+++ create_reads.py
@@ -0,0 +1,64 @@
+import argparse
+import shlex
+import subprocess
+
+
+# Class to parse arguments
+class Arguments:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description='Generate reads from a reference genome')
+        self.parser.add_argument('Sample 0', type=str, help='sample 0 fna file')
+        self.parser.add_argument('Sample 1', type=str, help='sample 1 fna file')
+        self.parser.add_argument('output_dir', type=str, help='Output Directory')
+        self.sample_0 = None
+        self.sample_1 = None
+        self.output_dir = None
+        self.parse_args()
+    
+    def parse_args(self):
+        args = self.parser.parse_args()
+        self.sample_0 = args.sample_0
+        self.sample_1 = args.sample_1
+        self.output_dir = args.output_dir
+    
+    def get_sample_0(self):
+        return self.sample_0
+
+    def get_sample_0(self):
+        return self.sample_0
+
+# ./dwgsim -1 100 -2 100 -y 0 sequence.fasta output
+# ./dwgsim -N 10000 -1 100 -2 100 -y 0 sequence.fasta output
+# ./dwgsim -N 10000 -1 100 -2 100 -y 0 sequence.fasta sample_output/output
+# ./dwgsim -h
+# ./dwgsim -n 10000 -1 100 -2 100 -y 0 sequence.fasta output
+# ./dwgsim -n 211720 -1 100 -2 100 -y 0 ./sequence.fasta output
+# ./dwgsim -1 100 -2 100 -y 0 -C 100 ./sequence.fasta output
+# ./dwgsim -1 100 -2 100 -y 0 -e 0 -E 0 -r 0 -C 100 ./sequence.fasta output
+# ./dwgsim -1 151 -2 151 -y 0 -C 100 ./sequence.fasta output
+# ./dwgsim -1 151 -2 151 -y 0 -e 0 -E 0 -r 0 -C 100 ./sequence.fasta output
+# ./dwgsim -N 211720 -1 151 -2 151 -y 0 -e 0 -E 0 -r 0 ./sequence.fasta output
+# ./dwgsim -N 105857200 -1 151 -2 151 -y 0 -e 0 -E 0 -r 0 ./sequence.fasta output
+# ./dwgsim -N 10,585720 -1 151 -2 151 -y 0 -e 0 -E 0 -r 0 ./sequence.fasta output
+# ./dwgsim -N 10585720 -1 151 -2 151 -y 0 -e 0 -E 0 -r 0 ./sequence.fasta output
+class DwgsimRunner:
+    def __init__(self, sample_0, output_dir):
+        self.sample_0 = sample_0
+        self.output_dir = output_dir
+
+    def generate_reads(self):
+        command = f"./dwgsim -1 100 -2 100 -y 0 -C 100 {self.sample_0} ${self.output_dir}/sample_0"
+        args = shlex.split(command)
+
+        result = subprocess.run(args, check=True)
+
+def main():
+    args = Arguments()
+    args.parse_args()
+    dwgsim_runner_sample_0 = DwgsimRunner(args.get_sample_0(), args.output_dir)
+    dwgsim_runner_sample_1 = DwgsimRunner(args.get_sample_1(), args.output_dir)
+    dwgsim_runner_sample_0.generate_reads()
+    dwgsim_runner_sample_1.generate_reads()
+
+if __name__ == "__main__":
+    main()
